44aa79fdcdf4a38aeacd6ff231fd4e94348c92a4
jrodriguezASC4 <joseph.rodriguez416@gmail.com>
2017-07-28 14:58:50 -0400

    created a personal website!

---
+++ JOSEPH-superstellarpage/app.js
@@ -0,0 +1,8 @@
+function setup() {
+    $('body').append("<h1>SUPER STELLAR, inc. (logo here)</h1>");
+    $('body').append('<div><h3>Consoles</h3><p id="consoles"></p></div>');
+    
+}
+
+// Wait until document is ready!!!
+$(document).ready(setup);
